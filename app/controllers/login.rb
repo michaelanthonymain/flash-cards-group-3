0a31fa7bf8e08c1a68dc7e786c73141a978d2687
Michael Main <michaelanthonymain@gmail.com>
2014-04-04 15:42:53 -0500

    Added login controller. Did not add register, logout, etc. Additionally added authenticate method to User model.

---
+++ app/controllers/login.rb
@@ -0,0 +1,61 @@
+enable :sessions
+
+# GET  /logout   // destroys session and redirects to /
+# GET  /login    // gets the webpage that has the login form
+# POST /login    // authenticates credentials against database and either redirects home with a new session or redirects back to /login
+# GET  /register // gets the webpage that has the registration form
+# POST /register // records the entered information into database as a new /user/xxx
+# GET  /user/:id // gets and renders current user data in a profile view
+# POST /user/:id // updates new information about user
+
+
+#loading basic root stuff, y'all know this
+get '/' do
+  # Look in app/views/index.erb
+  erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+#form from main page posts to /login.
+post '/login' do
+  puts "this is the session guy: #{session.inspect}"
+  if User.authenticate(params[:name], params[:password]) #authenticate is a User method that matches user input to database records
+    session[:name] = params[:name]
+    session[:password] = params[:password]
+    puts "Session set as #{session.inspect}"
+    redirect '/'
+  else
+    erb :login
+  end
+end
+
+#creates a new user.
+get '/register' do
+  erb :register #this is just a form that creates a new user
+end
+
+#Form from line 50 posts here, creates a new User object
+post '/register' do
+  User.create()
+  # erb :index
+end
+
+
+
+#just logs the user out, and clears the session. redirects to root.
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+# this relates to line 66 ... the actual GET call
+# get '/secret' do
+#   if session[:email] == nil
+#     redirect '/'
+#   else
+#     erb :secret_page
+#   end
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :decks
 	has_many :rounds
-	
+      validates :name, uniqueness: true
+
+    def self.authenticate(name, password)
+      if self.find(name: name).password == password
+        return true
+      else
+        return false
+      end
+    end
+
 end
