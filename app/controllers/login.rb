fc670afa2ab9273501aa1c81e57ca0636b455f75
Ron Gierlach <rongierlach@yahoo.com>
2014-04-04 22:15:19 -0500

    Added new_round and usrpage views --the new_round view is the view the user sees once they have selected a deck but before they have committed to playing a new game --the usrpage view is the view a user sees after they have logged in: giving them the option to start a new round (will add create a new deck later)

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -4,25 +4,14 @@ enable :sessions
 # POST /user/:id // updates new information about user
 
 
-#loading basic root stuff, y'all know this
-get '/' do
-  @name = params[:session][:name]
-
-  erb :index
-end
-
 get '/login' do
   erb :login
 end
 
-#form from main page posts to /login.
 post '/login' do
-  if User.authenticate(params[:name], params[:password]) #authenticate is a User method that matches user input to database records
+  if User.authenticate(params[:name], params[:password])
     session[:name] = params[:name]
     session[:password] = params[:password]
-    puts "!!!!!"
-    p session.inspect
-    puts "!!!!!"
     redirect '/usr'
   else
     erb :login
@@ -43,7 +32,6 @@ post '/register' do
   end
 end
 
-# #just logs the user out, and clears the session. redirects to root.
 get '/logout' do
   session.clear
   erb :logout
--- app/controllers/usr.rb
+++ app/controllers/usr.rb
@@ -1,3 +1,10 @@
 get '/usr' do
-  
+  @current_user = current_user
+  erb :'usr/usrpage'
 end 
+
+post '/usr/round' do
+  deck_id = params[:deck].to_i
+  @deck = Deck.find(deck_id) 
+  erb :'round/new_round'
+end
