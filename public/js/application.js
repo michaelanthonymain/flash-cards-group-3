afb27508fcb5f22ff707d968a12c84ea8db5bd27
Michael Main <michaelanthonymain@gmail.com>
2014-04-06 14:41:52 -0500

    Added validation errors to deck creation and card creation. Woot.

--- public/js/application.js
+++ public/js/application.js
@@ -19,6 +19,27 @@ $(document).ready(function() {
           return false;
         }
 
+        var deck_name = $("input#deck_name").val();
+          if (deck_name == "") {
+          $("label#deck_name_error").show();
+          $("input#deck_name").focus();
+          return false;
+        }
+
+        var question = $("input#question").val();
+          if (question == "") {
+          $("label#question_error").show();
+          $("input#question").focus();
+          return false;
+        }
+
+        var answer = $("input#answer").val();
+          if (answer == "") {
+          $("label#answer_error").show();
+          $("input#answer").focus();
+          return false;
+        }
+
     });
   });
 
