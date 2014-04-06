f8c4dcc0aeeb98fdef70e4ca78b3eabf6207330c
Ron Gierlach <rongierlach@yahoo.com>
2014-04-06 12:43:28 -0500

    Added deck and card creation/deletion for users. Still need to work on individual card and deck views.

--- app/controllers/usr.rb
+++ app/controllers/usr.rb
@@ -7,18 +7,12 @@ end
 
 get '/usr/play' do
   @user = User.find(session[:user_id])
-  puts '/usr/play'
-  p session
-  p params
   erb :play
 end
 
 post '/usr/round' do
   deck_id = params[:deck_id].to_i
   @deck = Deck.find(deck_id) 
-  puts "/usr/round"
-  p session
-  p params
   erb :'round/new_round'
 end
 
@@ -26,25 +20,53 @@ get '/usr/round/:deck_id' do
   @user = User.find(session[:user_id])
   @deck = Deck.find(params[:deck_id])
   @round = Round.create(user_id: @user.id, deck_id: @deck.id)
-  puts "/usr/round/:deck_id"
-  p session
-  p params
   erb :'round/live_round'
 end
 
+# DEC
+
 get '/usr/new_deck' do
+  @user = User.find(session[:user_id])
+  @decks = @user.decks 
   erb :make_deck
 end
 
 post '/usr/new_deck' do
+  p params
   @deck_name = params[:deck_name] 
-  redirect "/usr/new_deck/#{@deck_name}"
+  @deck = Deck.create!(name: @deck_name, user_id: session[:user_id])
+  p @deck
+  redirect "/usr/new_deck/#{@deck_name}/new_card"
 end
 
-get '/usr/new_deck/:deck_name' do
-  @deck_name = params[:deck_name]
-  @user = User.find(session[:user_id])
-  # @deck = Deck.create(name: @deck_name, )
+post '/usr/new_deck/:deck_id' do
+  @deck = Deck.find(params[:deck_id])
+  @deck.destroy
+  redirect "/usr/new_deck"
+end
+
+
+# CARD
+
+get '/usr/new_deck/:deck_name/new_card' do
+  @deck = Deck.where(name: params[:deck_name]).first 
+  @cards = @deck.cards 
   erb :make_card
 end
+
+post '/usr/new_deck/:deck_name/new_card' do 
+  @deck = Deck.where(name: params[:deck_name]).first
+  Card.create!(deck_id: @deck.id, question: params[:question], answer: params[:answer])
+  redirect "/usr/new_deck/#{@deck.name}/new_card"
+end
+
+post '/usr/new_deck/:deck_name/new_card/:card_id' do
+  @deck = Deck.where(name: params[:deck_name]).first
+  @card = Card.find(params[:card_id])
+  @card.destroy
+  redirect "/usr/new_deck/#{@deck.name}/new_card"
+end
+
+
+
 
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,16 +1,15 @@
 class Deck < ActiveRecord::Base
 	belongs_to :user 
 	has_many :cards 
-
+			validates :name, uniqueness: true
 	attr_accessor :cards_arr
 
 	def get_cards 
-		@cards_arr = self.cards.shuffle
+	 	@cards_arr = self.cards.shuffle
 	end
-
-	def pick_card
-		this_card = self.cards_arr.delete_at(0)
-		return this_card
+  def pick_card
+	 	this_card = self.cards_arr.delete_at(0)
+	 	return this_card
 	end
 end
 
