edabd5a8330afd49f39129491d13bee561b1e2af
Ron Gierlach <rongierlach@yahoo.com>
2014-04-05 20:08:38 -0500

    Removed data from session

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,22 +1,12 @@
 enable :sessions
 
-# GET  /user/:id // gets and renders current user data in a profile view
-# POST /user/:id // updates new information about user
-
-
 get '/login' do
   erb :login
 end
 
 post '/login' do
-  # login_information = params[:name]
   if User.authenticate(params[:name], params[:password])
-    # session[:name] = params[:name]
-    # session[:password] = params[:password]
-    puts "!!!!!!user created in session"
-    p current_user
-    session[:user] = current_user
-    p session
+    session[:user_id] = current_user.id
     redirect '/usr'
   else
     erb :login
@@ -28,11 +18,10 @@ get '/register' do
 end
 
 post '/register' do
-  p params
   @new_user = User.new(name: params[:name], password: params[:password])
   if @new_user.save
-    session[:user] = current_user
-    redirect '/usr' #this should probably lead to the users page that Ron is writing.
+    session[:user_id] = current_user.id
+    redirect '/usr' 
   else
     erb :register
   end
--- app/controllers/usr.rb
+++ app/controllers/usr.rb
@@ -1,23 +1,50 @@
 enable :sessions
 
 get '/usr' do
-  @user = session[:user]
-  p @user
-  p session
+  @user = User.find(session[:user_id])
   erb :'usr/usrpage'
 end 
 
+get '/usr/play' do
+  @user = User.find(session[:user_id])
+  puts '/usr/play'
+  p session
+  p params
+  erb :play
+end
+
 post '/usr/round' do
-  deck_id = params[:deck].to_i
+  deck_id = params[:deck_id].to_i
   @deck = Deck.find(deck_id) 
-  session[:deck] = @deck
+  puts "/usr/round"
+  p session
+  p params
   erb :'round/new_round'
 end
 
 get '/usr/round/:deck_id' do 
-  @user = session[:user]
-  @deck = session[:deck]
-  p session
+  @user = User.find(session[:user_id])
+  @deck = Deck.find(params[:deck_id])
   @round = Round.create(user_id: @user.id, deck_id: @deck.id)
+  puts "/usr/round/:deck_id"
+  p session
+  p params
   erb :'round/live_round'
 end
+
+get '/usr/new_deck' do
+  erb :make_deck
+end
+
+post '/usr/new_deck' do
+  @deck_name = params[:deck_name] 
+  redirect "/usr/new_deck/#{@deck_name}"
+end
+
+get '/usr/new_deck/:deck_name' do
+  @deck_name = params[:deck_name]
+  @user = User.find(session[:user_id])
+  # @deck = Deck.create(name: @deck_name, )
+  erb :make_card
+end
+
